8d6708f18007456a33c427fc309067eb8fcabc6b
Faridha <46135349+faridhafaree@users.noreply.github.com>
2019-06-10 10:44:47 +0530

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,3 @@
+create table login (users varchar(10), pass varchar(10));
+Insert into login (users,pass) values('aziz', 1234);
+create table image_table(aid number(25) primary key, image long raw, age NUMBER(3), occu varchar(20), gender varchar(8), personality varchar(20), status varchar(10), uname varchar(20));
